92b1d8704393124fa3c161b2aea4563079e1418d
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-07-28 10:07:13 +0530

    for ASCP

--- berlna.mk
+++ berlna.mk
@@ -11,10 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from berlna device
 $(call inherit-product, device/motorola/berlna/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ASCP stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_berlna
+# Flags
+WITH_REVANCED := true
+ASCP_MAINTAINER := Shipu
+
+PRODUCT_NAME := berlna
 PRODUCT_DEVICE := berlna
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
